9cd26872a00c3875a5b3c986d0b5fd59cc4aa1c7
rajesh171812 <80300745+rajesh171812@users.noreply.github.com>
2026-09-20 22:17:50 -0400

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Explicitly tell Next.js where the app lives
-  // (required when using src/ directory on some Vercel configs)
-};
-
-module.exports = nextConfig;
