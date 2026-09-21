811fbdceb73cdb20ea469eb86b67b145ff63847b
rajesh171812 <80300745+rajesh171812@users.noreply.github.com>
2026-09-20 22:17:02 -0400

    Delete src directory

--- src/app/page.js
+++
@@ -1,5 +0,0 @@
-import SubTrack from "../components/subtrack/SubTrack";
-
-export default function Page() {
-  return <SubTrack />;
-}
--- src/app/page.jsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import App from "../components/SubDashboard";
-
-export default function Home() {
-  return <App />;
-}
