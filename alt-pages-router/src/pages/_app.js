45f888ef8492e2bfb7998ae6df213e9eb3d1545e
rajesh171812 <80300745+rajesh171812@users.noreply.github.com>
2026-09-20 22:15:38 -0400

    Delete alt-pages-router/src/pages directory

--- alt-pages-router/src/pages/_app.js
+++
@@ -1,8 +0,0 @@
-import "../components/subtrack/tokens.css";
-import "../components/subtrack/bundle.css";
-import "../components/subtrack/subtrack-theme.css";
-import "../components/subtrack/subtrack-app.css";
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
